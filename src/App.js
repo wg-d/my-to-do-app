82cc4c7a1d275ea0c9e0ff0c11038f0b25dedc88
wg-d <wonderdream.dev@outlook.com>
2022-04-19 09:35:06 +0900

    state updates from child form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React, {Component} from 'react'
 import Table from './Table'
-
+import Form from './Form'
 
 class App extends Component {
+  state = {
+    characters: [
+    ]
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
   render() {
-    const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-    ];
+  
+    const { characters } = this.state
+
     return (
       <div className="container">
         <h1>Hello, React!</h1>
-        <Table characterData={characters}/>
+        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ const TableHeader = () => {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Action</th>
         </tr>
       </thead>
     )
@@ -17,6 +18,9 @@ const TableBody = (props) => {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
         </tr>
       )
     })
@@ -25,17 +29,15 @@ const TableBody = (props) => {
 }
 
 
-class Table extends Component {
-  render() {
-    const {characterData} = this.props
+const Table = (props) => {
+  const {characterData, removeCharacter} = props
 
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={characterData}/>
-      </table>
-    )
-  }
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
 
 export default Table
